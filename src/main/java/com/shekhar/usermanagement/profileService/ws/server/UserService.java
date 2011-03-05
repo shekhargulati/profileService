286fdfcb4525c1c4f9da605cffe3eebafdd3c2c8
shekhar <shekhargulati84@gmail.com>
2011-03-05 17:37:05 +0530

    renamed package to .server

---
+++ src/main/java/com/shekhar/usermanagement/profileService/ws/server/UserService.java
@@ -0,0 +1,9 @@
+package com.shekhar.usermanagement.profileService.ws.server;
+
+import com.shekhar.usermanagement.profile.UserProfile;
+
+public interface UserService {
+
+	String createUser(UserProfile userProfile);
+
+}
