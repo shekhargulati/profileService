4ed753bfbc67f80a617b6db96917266460334ea0
shekhar <shekhargulati84@gmail.com>
2011-03-05 17:50:13 +0530

    deleted files

--- src/main/java/com/shekhar/usermanagement/profileService/ws/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.shekhar.usermanagement.profileService.ws;
-
-import com.shekhar.usermanagement.profile.UserProfile;
-
-public interface UserService {
-
-	String createUser(UserProfile userProfile);
-
-}
